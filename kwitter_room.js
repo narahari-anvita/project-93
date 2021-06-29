98fd17ebf8d3434953d24138f9145123b3cd09fb
narahari-anvita <79611794+narahari-anvita@users.noreply.github.com>
2021-06-29 11:33:44 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAB_VPYB9oLJUI_3t8siFRaejY6pLRYOmE",
+    authDomain: "letschat-c12dd.firebaseapp.com",
+    databaseURL: "https://letschat-c12dd-default-rtdb.firebaseio.com",
+    projectId: "letschat-c12dd",
+    storageBucket: "letschat-c12dd.appspot.com",
+    messagingSenderId: "776933976424",
+    appId: "1:776933976424:web:2ee1cd660110d3b39d0edd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
